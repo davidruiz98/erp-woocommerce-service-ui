e0a4d0c024b9c13eaf0f819cb04eee847236cea0
davidruiz98 <66329086+davidruiz98@users.noreply.github.com>
2025-04-18 16:14:06 -0400

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 echo "Reemplazando \$PORT en nginx.template.conf"
-envsubst '\$PORT' < /etc/nginx/nginx.template.conf > /etc/nginx/nginx.conf
+envsubst '$PORT' < /etc/nginx/nginx.template.conf > /etc/nginx/nginx.conf
 
 echo "Reemplazando \$API_URL en env.template.js"
-envsubst '\$API_URL' < /usr/share/nginx/html/assets/env.template.js > /usr/share/nginx/html/assets/env.js
+envsubst '$API_URL' < /usr/share/nginx/html/assets/env.template.js > /usr/share/nginx/html/assets/env.js
 
 echo "Iniciando Nginx en el puerto $PORT"
 exec "$@"
