5120b34b42dfc004ec8d43fdb515cfa91f141fa8
davidruiz98 <66329086+davidruiz98@users.noreply.github.com>
2025-04-17 21:24:24 -0400

    Update productos-list.component.ts

--- src/app/productos/productos-list/productos-list.component.ts
+++ src/app/productos/productos-list/productos-list.component.ts
@@ -4,7 +4,6 @@ import { MatTableModule } from '@angular/material/table';
 import { MatButtonModule } from '@angular/material/button';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
-import { ProductoPreciosComponent } from '../producto-precios/producto-precios.component';
 import { ProductosService } from '../productos.service';
 import { Product } from '../../productos/productos/product.interface';
 
