63c7dca342b744f423a494ec9c37e87002626de4
davidruiz98 <66329086+davidruiz98@users.noreply.github.com>
2025-04-18 11:37:02 -0400

    mod navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,17 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { AuthService } from './auth/auth.service';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
+import { MatButtonModule } from '@angular/material/button';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterModule],
-  templateUrl: './app.component.html'
+  imports: [CommonModule, RouterModule, MatIconModule,    MatToolbarModule,
+    MatButtonModule ],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   constructor(public authService: AuthService) {}
